7c0c2b4d7d867f941c1c1e645fdc72dd85b36837
Malkhozov Ruslan <mr.ruslan.ghost@gmail.com>
2019-03-30 11:50:53 +0300

    Update lib2.c

--- lib2.c
+++ lib2.c
@@ -10,20 +10,26 @@ void main (void){
     while(i < N){
         printf("Vvedite a[%d]", i+1);
         scanf("%f", &a[i]);
-        if(a[i]>0)
-            min = a[i];
         i++;
     }
 
     i = 0;
     while(i < N){
-        if(a[i] < min && a[i] > 0)
+        if(a[i] > 0)
             min = a[i];
         i++;
     }
 
     if(min==0.)
         printf("Takih chisel ne sushchestvuet");
-    else
-        printf("min = %f", min);
+    else{
+        i = 0;
+        while(i < N){
+            if(a[i] < min && a[i] > 0)
+                min = a[i];
+            i++
+        }
+    printf("min = %f", min);
+    }
+        
 }
